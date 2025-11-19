8e56c82f3dbe94ddcd83041d1a24df74356bac26
azim <centrsistema@bk.ru>
2025-11-19 21:19:48 +0500

    fix: disable cors

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://api.example.com/:path*',
+        destination: 'https://trading-view-gilt.vercel.app/:path*',
       },
     ];
   },
